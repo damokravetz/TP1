2210e0812dbdf0ee1c8d2559db3ea31fc34d5c16
damokravetz <kravetzdamian@gmail.com>
2019-04-11 14:16:38 -0300

    ejercicio 6 v2

--- Tp1/src/ar/edu/ort/taller/ejer6/Mes.java
+++ Tp1/src/ar/edu/ort/taller/ejer6/Mes.java
@@ -1,5 +1,8 @@
 package ar.edu.ort.taller.ejer6;
 
 public enum Mes {
+	
+		ENERO, FEBRERO, MARZO, ABRIL, MAYO, JUNIO, JULIO, AGOSTO, SEPTIEMBRE, OCTUBRE, NOVIEMBRE, DICIEMBRE;
+	
 
 }
--- Tp1/src/ar/edu/ort/taller/ejer6/Rubro.java
+++ Tp1/src/ar/edu/ort/taller/ejer6/Rubro.java
@@ -1,5 +1,31 @@
 package ar.edu.ort.taller.ejer6;
 
 public class Rubro {
+	private String nombre;
+	private static final int TOTAL_MESES = 12;
+	private double[] gastosPorMes;
 
+	public Rubro(String nombre) {
+		this.nombre = nombre;
+		inicializarGastos();
+	}
+	
+	private void inicializarGastos() {
+		this.gastosPorMes = new double[12];
+		for (int i = 0; i < gastosPorMes.length; i++) {
+			gastosPorMes[i]=0;
+		}
+	}
+	
+	public void agregarGasto(Mes mes, double importe) {
+		gastosPorMes[mes.ordinal()] += importe; 
+	}
+	
+	public double getTotalGastos(Mes mes) {
+		return gastosPorMes[mes.ordinal()];
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
 }
